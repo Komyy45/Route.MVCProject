c554763fa605a1fadc2add636b39143d2f803013
Youssef Elkomy <youssefelkomy500@gmail.com>
2024-09-10 00:56:38 +0300

    Part 08 3-Layers Arch. Pattern - Business Logic Layer

---
+++ Route.MVCProject.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Route.MVCProject.DAL.Models;
+
+namespace Route.MVCProject.BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+
+        Department Get(int id);
+
+        int Add(Department entity);
+
+        int Update(Department entity);
+
+        int Delete(Department entity);
+    }
+}
